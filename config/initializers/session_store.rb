8cf7e92cd608598ee228a1c170e49bc3ab671db9
diego576 <diegomoreno576@gmail.com>
2022-07-27 00:21:35 +0200

    fixed sesions2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
- Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "notecopies.herokuapp.com"
+Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+Rails.application.config.middleware.use ActionDispatch::Cookies
+Rails.application.config.middleware.use config.session_store, config.session_options
