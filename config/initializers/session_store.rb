aaaef3fa1d21ee04de97dc4fd5b0fc8486088ab4
diego576 <diegomoreno576@gmail.com>
2022-07-27 00:34:56 +0200

    fixed sesions2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,9 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://notecopies.herokuapp.com"
-else
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://notecopies.herokuapp.com"
-end
+# if Rails.env == "production"
+#     Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://notecopies.herokuapp.com/"
+# else
+#     Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://notecopies.herokuapp.com/"
+# end
+
+Rails.application.config.middleware.use ActionDispatch::Cookies
+Rails.application.config.middleware.use ActionDispatch::Session::CookieStore, key: '_your_app'
+Rails.application.config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore, key: '_your_app')
